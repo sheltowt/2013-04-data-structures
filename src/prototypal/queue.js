ae2a6398c2bcb16453f36a77e7e035c203495448
Hack Reactor Students <github@hackreactor.com>
2013-04-29 18:25:33 -0700

    Prototype completed

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,1 +1,36 @@
 // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+// Hey! Copy your code from src/functional/queue.js and paste it here
+var Queue = function(){
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  // Implement the methods below
+  var queue = {};
+  var queue = Object.create(masterFunctionsQueuePrototype);
+  queue.last = 0;
+  queue.first = 0;
+  return queue;
+};
+
+var masterFunctionsQueuePrototype = {
+  add : function(value){
+    this[this.last] = value;
+    this.last ++;
+    return this;
+  },
+  remove : function(){
+    var returnVariable = this[this.first];
+    delete this[this.first];
+    this.first ++;
+    return returnVariable;
+  },
+  size : function(){
+    var sizeCheck = 0;
+    for(var key in this){
+        if(!isNaN(key)){
+          sizeCheck = sizeCheck + 1;
+        }
+    }
+    return sizeCheck;
+  }
+};
--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,1 +1,31 @@
 // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+var Stack = function(){
+  var storage = {};
+
+  // Implement the methods below
+  var stack = Object.create(masterFunctionsStack);
+  stack.last = 0;
+  return stack;
+};
+
+var masterFunctionsStack = {
+  add : function(value){
+    this[this.last] = value;
+    this.last ++;
+  },
+  remove : function(){
+    var length = this.size();
+    var returnVariable = this[length-1];
+    delete this[length-1];
+    return returnVariable;
+  },
+  size : function(){
+    var sizeCheck = 0;
+    for(var key in this){
+        if(!isNaN(key)){
+          sizeCheck = sizeCheck + 1;
+        }
+    }
+    return sizeCheck;
+  }
+};
